1205c8b1a749cd4cc0cb39722553485eceec7136
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2026-09-08 18:16:38 -0400

    Add Softmax to ProjectionLayer output
    Apply Softmax activation to the projection layer output.

--- transformer_owen/decoder_transformer.py
+++ transformer_owen/decoder_transformer.py
@@ -168,6 +168,7 @@ class ProjectionLayer(nn.Module):
     def __init__(self, d_model, vocab_size):
         super().__init__()
         self.proj = nn.Linear(d_model, vocab_size)
+        self.proj = nn.Softmax(self.proj, dim=1)
         
     def forward(self, x):
         return self.proj(x)
