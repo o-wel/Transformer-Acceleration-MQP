5f20403efdc0613e9c78116b867b245978a24fa9
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2026-09-08 17:50:03 -0400

    Implement softmax, attention, and GPU setup
    Added softmax and attention methods, and device setup for GPU.

--- transformer_syed/transformer.py
+++ transformer_syed/transformer.py
@@ -1,12 +1,31 @@
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 
 class Transformer():
-
+    
     def linear(self):
 
     def feedforward(self):
 
     def softmax(self,X):
+        for i in range(len(X)):
+            summation = sum(np.exp(X[i]))
+            for j in range(len(X[i])):
+                X[i][j] = np.exp(X[i][j])/summation
+
+
 
     def attention(self,Q,K,V):
+        QK = (Q * K.T)/ndim
+        return softmax(QK)*V
+
+def forward():
+    model = Transformer().to(device)
+
+
+def setGPUInTorch():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    return device
+
