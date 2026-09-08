704aadc1e7390226b5f0b389ac79d6b61a5804ec
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2026-09-08 17:20:27 -0400

    Add linear and feedforward methods to Transformer
    Added linear and feedforward methods to the Transformer class.

--- transformer_syed/transformer.py
+++ transformer_syed/transformer.py
@@ -1,8 +1,12 @@
 import torch
-
+import torch.nn as nn
 
 class Transformer():
-    
-    def softmax(X):
 
-    def attention(Q,K,V):
+    def linear(self):
+
+    def feedforward(self):
+
+    def softmax(self,X):
+
+    def attention(self,Q,K,V):
